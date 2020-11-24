93ee48decaff5ac7b6317b727ef7b777a069be47
Maryam Chaudhry <maryamc1798@gmail.com>
2020-11-24 14:47:20 -0500

    written basic schema code for sql complete, not the bonus

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,26 +1,28 @@
--- DROP DATABASE IF EXISTS employeetracker_db;
+DROP DATABASE IF EXISTS employeetracker_db;
 
--- CREATE DATABASE employeetracker_db;
+CREATE DATABASE employeetracker_db;
 
--- USE employeetracker_db;
+USE employeetracker_db;
 
--- CREATE TABLE department (
---     id INT AUTO_INCREMENT NOT NULL,
---     name VARCHAR(30) NOT NULL,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE roles (
---     id INT AUTO_INCREMENT NOT NULL,
---     title VARCHAR(30) NOT NULL,
---     salary DECIMAL(10,2),
---     department_id INT (10)
--- );
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INT (10),
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE employee (
---     id INT AUTO_INCREMENT NOT NULL,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
-
--- )
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT (10),
+    PRIMARY KEY (id)
+);
 
