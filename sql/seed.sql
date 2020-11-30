40e2de618f502aa61a116b589d5fc5306f3972a9
Maryam Chaudhry <maryamc1798@gmail.com>
2020-11-29 21:10:09 -0500

    seeding in for employee and roles

--- sql/seed.sql
+++ sql/seed.sql
@@ -12,12 +12,24 @@ INSERT INTO department (name)
 VALUES ("Finance");
 
 -- Roles
-
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Engineer", "170000", "1");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Marketing Director", "124000", "2");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Human Resources Manager", "95000", "3");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Financial Analyst", "150000", "4");
 
 -- Employees
 INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Leeroy", "Jenkins", "null");
-
+VALUES ("Leeroy", "Jenkins", "1");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Ella", "Vader", "2");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Levy", "Tate", "3");
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Olive", "Garden", "4");
 -- INSERT INTO colleges (name)
 -- VALUES ("Boston College");
 
